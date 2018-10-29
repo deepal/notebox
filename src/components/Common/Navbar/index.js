ec7b1409ffd08b9bbb4ac24ca65164b18a83174d
Deepal Jayasekara <dpjayasekara@gmail.com>
2018-10-29 23:36:07 +0100

    change landing page styles

--- src/components/Common/Navbar/index.js
+++ src/components/Common/Navbar/index.js
@@ -1,24 +1,46 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 class Navbar extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return (
-            <nav className="notebox-navbar">
+        const loggedInUser = this.props.loggedInUser;
+        let navbarContent;
+
+        if (loggedInUser) {
+            const {firstName, lastname} = loggedInUser;
+            navbarContent = (
                 <div className="nav-wrapper">
-                    <a href="#!" className="brand-logo">Note Box</a>
+                    <a href="/" className="brand-logo">Note Box</a>
                     <ul className="right hide-on-med-and-down">
                         <li><a href="/create-note">Create Note</a></li>
                         <li><a href="/create-notebox">Create Note Box</a></li>
-                        <li><a href="/">Deepal Jayasekara <i className="material-icons right white-text">exit_to_app</i></a></li>
+                        <li><a href="/">{firstName} {lastname} <i className="material-icons right white-text">exit_to_app</i></a></li>
                     </ul>
                 </div>
+            )
+        } else {
+            navbarContent = (
+                <div className="nav-wrapper">
+                    <ul className="right hide-on-med-and-down">
+                        <li><a href="/login">Log In<i className="material-icons right white-text">person</i></a></li>
+                    </ul>
+                </div>
+            )
+        }
+
+        return (
+            <nav className="notebox-navbar">
+                {navbarContent}
             </nav>
         );
     }
+}
+
+Navbar.propTypes = {
+    loggedInUser: PropTypes.object
 }
 
 export default Navbar;
